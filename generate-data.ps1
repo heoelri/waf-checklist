95abd35f1cd648a826a1a414e47f720861096c75
heoelri <heoelri@microsoft.com>
2021-03-10 14:59:20 +0100

    switch  back to pillars

--- generate-data.ps1
+++ generate-data.ps1
@@ -3,7 +3,7 @@
           
           
 $AllItems = @()
-Get-ChildItem -Path "en/items" | where { $_.Name -match ".data.json$" } | % {
+Get-ChildItem -Path "input/" | where { $_.Name -match ".data.json$" } | % {
     Get-Content -Path $_.FullName | ConvertFrom-Json | %{
         if($_) 
         {
@@ -19,9 +19,9 @@ $AllPillars | % {
     Write-Host "Combine data for $currentPillar pillar"
 
     $AllItems | % {
-        $_ | Add-Member -MemberType NoteProperty -Name tags -Value @("all", $_.subCategory) -Force
+        $_ | Add-Member -MemberType NoteProperty -Name tags -Value $_.pillars -Force
         $_ | Add-Member -MemberType NoteProperty -Name priority -Value "medium" -Force
     }
 
-    $AllItems | Where-Object { $_.pillars -contains $currentPillar } | ConvertTo-Json -depth 16 | Set-content "en/items/$currentPillar.json"
+    $AllItems | Where-Object { $_.pillars -contains $currentPillar } | ConvertTo-Json -depth 16 | Set-content "data/en/items/$currentPillar.json"
 }
