4a28084ee20c3acbe5694979ff75fe209810336c
heoelri <heoelri@microsoft.com>
2021-03-10 15:24:32 +0100

    switch tag to category

--- generate-data.ps1
+++ generate-data.ps1
@@ -19,7 +19,7 @@ $AllPillars | % {
     Write-Host "Combine data for $currentPillar pillar"
 
     $AllItems | % {
-        $_ | Add-Member -MemberType NoteProperty -Name tags -Value $_.pillars -Force
+        $_ | Add-Member -MemberType NoteProperty -Name tags -Value @("all", $_.category) -Force
         $_ | Add-Member -MemberType NoteProperty -Name priority -Value "medium" -Force
     }
 
